3076b4e4cc5c6c9a26a1d04047c3697dd38dc7bf
Pavlo <pasha2708@gmail.com>
2023-05-07 15:23:00 +0300

    add topic

--- Schema.js
+++ Schema.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const schema = new mongoose.Schema({
   date: { type: String, required: true },
+  topic: { type: String },
   text: { type: String, required: true },
 })
 
